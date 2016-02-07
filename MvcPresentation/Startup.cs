38fa1523bacbd2d002969464651457d4a5072426
polycirrus <polycirrus@gmail.com>
2016-02-07 22:36:23 +0300

    Lay out solution architecture

---
+++ MvcPresentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetwork.MvcPresentation.Startup))]
+namespace SocialNetwork.MvcPresentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
