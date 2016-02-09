f85e65f48cf58058e9bf48ecba0d9d69c30b15b7
polycirrus <polycirrus@gmail.com>
2016-02-09 12:07:25 +0300

    Add data access-level entites

---
+++ DataAccess.EntityFramework/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DataAccess.EntityFramework.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Inhabitants { get; set; }
+    }
+}
--- DataAccess.EntityFramework/Entities/User.cs
+++ DataAccess.EntityFramework/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,23 @@ namespace SocialNetwork.DataAccess.EntityFramework.Entities
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+
+        [Required]
+        public virtual ICollection<Role> Roles { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Bio { get; set; }
+
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
+
+        public virtual ICollection<Message> MessagesFrom { get; set; }
+        public virtual ICollection<Message> MessagesTo { get; set; }
     }
 }
